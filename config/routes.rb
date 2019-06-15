4f5ad15a508efc965711eada80a6d76f64dd0282
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-15 15:14:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'store_admin/home'
+
+  get 'static/home'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
